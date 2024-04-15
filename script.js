162d5759eaacc12a7d1d14d4ccb22d4fcaa707cd
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-04-15 11:03:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function firstWord(s) {
+  // your code here
+	return s.toUpperCase()
+}
+
+// Do not change the code below
+const s = prompt("Enter s:");
+alert(firstWord(s));
